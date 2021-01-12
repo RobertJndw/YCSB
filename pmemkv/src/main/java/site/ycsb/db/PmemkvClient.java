69fe1b4f747027955d68cd1d14d5dd96e5a9ce7a
Robert Jandow <robert.jandow@tum.de>
2021-01-12 11:53:30 -0800

    Fix dependency problem with pmemkv

--- pmemkv/src/main/java/site/ycsb/db/PmemkvClient.java
+++ pmemkv/src/main/java/site/ycsb/db/PmemkvClient.java
@@ -6,15 +6,30 @@ import site.ycsb.DBException;
 import site.ycsb.Status;
 
 import java.util.*;
+import io.pmem.pmemkv.Database;
 
 /**
  * The YCSB binding for <a href="https://github.com/pmem/pmemkv">pmemkv</a>.
  */
 public class PmemkvClient extends DB {
 
+  private Database<String, Map<String, ByteIterator>> db;
+
   @Override
   public void init() throws DBException {
+    Properties props = getProperties();
 
+    String path = props.getProperty("path", "/dev/shm");
+    String engine = props.getProperty("engine", "vsmap");
+
+    try {
+      db = new Database.Builder<String, Map<String, ByteIterator>>(engine)
+          .setPath(path)
+          .build();
+    } catch (Exception e) {
+      System.err.println("Error: Creating the database failed");
+      throw new DBException(e);
+    }
   }
 
   @Override
